51efd6b260442247c7de1ab44b4b1e2cfb38bed5
praveenkumar <praveenkumarnaidu88@gmail.com>
2025-06-05 16:15:14 +0530

    completed user backend

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User";
+import User from "../models/User.js";
 import asyncHandler from "./asyncHandler.js";
 
 // check if the user id authenticated or not
@@ -9,7 +9,7 @@ const authenticate = asyncHandler(async (req, res, next) => {
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.userID).select("-password");
+      req.user = await User.findById(decoded.userId).select("-password");
       next();
     } catch (error) {
       res.status(401);
@@ -25,7 +25,7 @@ const authorizeAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    req.status(401).send("Not authorized as an Admin.");
+    res.status(401).send("Not authorized as an Admin.");
   }
 };
 
