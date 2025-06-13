0c969720478e4ae64a14a254dd3ee255fcb52105
praveenkumar <praveenkumarnaidu88@gmail.com>
2025-06-13 23:39:07 +0530

    added upload image functionality

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import genreRoutes from "./routes/genreRoutes.js";
 import moviesRoutes from "./routes/moviesRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 // configuration
 dotenv.config();
 connectDB();
@@ -26,6 +27,10 @@ const PORT = process.env.PORT;
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/genre", genreRoutes);
 app.use("/api/v1/movies", moviesRoutes);
+app.use("/api/v1/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 // server
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
