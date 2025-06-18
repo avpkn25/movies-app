8baad664ea9c11431883d4f30e979404a3c73f14
praveenkumar <praveenkumarnaidu88@gmail.com>
2025-06-18 10:03:55 +0530

    cleared localstorage value

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,13 @@ import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navigation from "./pages/Auth/Navigation";
+import { useEffect } from "react";
+
 
 const App = () => {
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
   return (
     <>
       <ToastContainer />
